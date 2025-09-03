ea270d6f2ec339a175a9ca83205e2e8b0df8b575
schipet <schillpeet@gmail.com>
2025-09-03 16:59:25 +0200

    enables to execute the script in any folder

--- brew-update-upgrade-cleanup-doctor.sh
+++ brew-update-upgrade-cleanup-doctor.sh
@@ -1,15 +1,17 @@
- #!/bin/sh
+#!/bin/sh
 
-figlet -d ./fonts -f "Bloody" -w 140 "breeeeeew..." | lolcat
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+figlet -d "$SCRIPT_DIR/fonts" -f "Bloody" -w 140 "breeeeeew..." | lolcat
 
 brew update
-figlet -d ./fonts -f "Bloody" -w 140 "updated \!\!\!" | lolcat
+figlet -d "$SCRIPT_DIR/fonts" -f "Bloody" -w 140 "updated \!\!\!" | lolcat
 
 brew upgrade
-figlet -d ./fonts -f "Bloody" -w 140 "upgraded \!\!\!" | lolcat
+figlet -d "$SCRIPT_DIR/fonts" -f "Bloody" -w 140 "upgraded \!\!\!" | lolcat
 
 brew cleanup
-figlet -d ./fonts -f "Bloody" -w 140 "cleaned up \!\!\!" | lolcat
+figlet -d "$SCRIPT_DIR/fonts" -f "Bloody" -w 140 "cleaned up \!\!\!" | lolcat
 
-figlet -d ./fonts -f "Bloody" -w 95 "What's up, Doc" | cowsay -f bunny -n | lolcat
+figlet -d "$SCRIPT_DIR/fonts" -f "Bloody" -w 95 "What's up, Doc" | cowsay -f bunny -n | lolcat
 brew doctor
